7de2441df526ae7157e8867be0d96f3d97cd86d8
kevas007 <kevassiobo@gmail.com>
2021-10-03 14:41:23 +0200

    kev

---
+++ resources/views/fontend/partials/header/header.blade.php
@@ -0,0 +1,111 @@
+V<div class="loader loader3">
+    <div class="loader-inner">
+      <div class="spin">
+        <span></span>
+        <img src="{{ asset('images/logo.svg') }}" alt="Hotel Himara">
+      </div>
+    </div>
+  </div>
+  <!-- ========== MOBILE MENU ========== -->
+  <nav id="mobile-menu"></nav>
+  <!-- ========== WRAPPER ========== -->
+  <div class="wrapper">
+    <!-- ========== TOP MENU ========== -->
+    <div class="topbar">
+      <div class="container">
+        <div class="welcome-mssg">
+          Welcome to Hotel Himara.
+        </div>
+        <div class="top-right-menu">
+          <ul class="top-menu">
+            <li class="d-none d-md-inline">
+              <a href="tel:+18881234567">
+                <i class="fa fa-phone"></i>+1 888 123 4567
+              </a>
+            </li>
+            <li class="d-none d-md-inline">
+              <a href="mailto:contact@hotelhimara.com">
+                <i class="fa fa-envelope-o "></i>contact@hotelhimara.com</a>
+            </li>
+            <li class="language-menu">
+              <a href="#" class="active-language"><img src="{{ asset('images/icons/flags/gb.png') }}" alt="Image">English</a>
+              <ul class="languages">
+                <li class="language">
+                  <a href="#"><img src="{{ asset('images/icons/flags/it.png') }}" alt="Image">Italiano</a>
+                </li>
+                <li class="language">
+                  <a href="#"><img src="{{ asset('images/icons/flags/gr.png') }}" alt="Image">Ελληνικα</a>
+                </li>
+                <li class="language">
+                  <a href="#"><img src="{{ asset('images/icons/flags/al.png') }}" alt="Image">Shqip</a>
+                </li>
+                <li class="language">
+                  <a href="#"><img src="{{ asset('images/icons/flags/fr.png') }}" alt="Image">Français</a>
+                </li>
+                <li class="language">
+                  <a href="#"><img src="{{ asset('images/icons/flags/es.png') }}" alt="Image">Español</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- ========== HEADER ========== -->
+    <header class="horizontal-header sticky-header" data-menutoggle="991">
+      <div class="container">
+        <!-- BRAND -->
+        <div class="brand">
+          <div class="logo">
+            <a href="index.html">
+              <img src="images/logo.svg" alt="Hotel Himara">
+            </a>
+          </div>
+        </div>
+        <!-- MOBILE MENU BUTTON -->
+        <div id="toggle-menu-button" class="toggle-menu-button">
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+        </div>
+        <!-- MAIN MENU -->
+        <nav id="main-menu" class="main-menu">
+          <ul class="menu">
+            <li class="menu-item dropdown active">
+              <a href="index.html">HOME</a>
+            </li>
+            <li class="menu-item dropdown">
+              <a href="rooms-list.html">ROOMS</a>
+            </li>
+            <li class="menu-item dropdown">
+              <a href="staff.html">TEAM</a>
+            </li>
+            <li class="menu-item dropdown">
+              <a href="gallery.html">GALLERY</a>
+            </li>
+            <li class="menu-item">
+              <a href="contact.html">CONTACT US</a>
+            </li>
+            <li class="menu-item dropdown">
+              <a href="#">ELEMENTS</a>
+              <ul class="submenu">
+                <li class="menu-item">
+                  <a href="style-guide.html">Style Guide</a>
+                </li>
+                <li class="menu-item">
+                  <a href="buttons.html">Buttons</a>
+                </li>
+                <li class="menu-item">
+                  <a href="icons.html">Icons</a>
+                </li>
+              </ul>
+            </li>
+            <li class="menu-item menu-btn">
+              <a href="booking-form.html" class="btn">
+                <i class="fa fa-user"></i>
+                LOG IN</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('fontend.layouts.main')
+@section('content')
+@include('fontend.partials.header.header')
+@include('fontend.partials.home.index')
+@include('fontend.partials.footer.footer')
+
+@endsection
